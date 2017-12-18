0599148b8d408a28868d115b5e47ba7c7ce8fcf3
nikson <nikson.barth@benner.com.br>
2017-12-18 12:52:30 -0200

    aula 2

--- main.js
+++ main.js
@@ -2,20 +2,41 @@ const app = new Vue({
     el: "#app",
     data: {
       bobby: {
-        name: "Bobby Boone",
+        first: "Bobby",
+        last: "Boone",
         age: 25
       },
       john: {
-        name: "John Boby",
+        first: "John",
+        last: "Boby",
         age: 35,
       }
     },
+    computed: {
+        bobbyFullName(){
+            return `${this.bobby.first} ${this.bobby.last}`
+        },
+        johnFullName(){
+            return `${this.john.first} ${this.john.last}`
+        },
+        johnAgeInOneYear(){
+            return this.john.age + 1;
+        }
+    },
+    filters:{
+        fullName(value){
+            return `${value.first} ${value.last}`;
+        },
+        ageInOneYear(age){
+            return age + 1;
+        }
+    },
     template: `
       <div>
-        <h2>Name: {{john.name}}</h2>
-        <h2>Age: {{john.age}}</h2>
-        <h2>Name: {{bobby.name}}</h2>
-        <h2>Age: {{bobby.age}}</h2>
+        <h2>Name: {{john | fullName}}</h2>
+        <h2>Age: {{john.age | ageInOneYear}}</h2>
+        <h2>Name: {{bobby | fullName}}</h2>
+        <h2>Age: {{bobby.age | ageInOneYear}}</h2>
       </div>
     `
   })
