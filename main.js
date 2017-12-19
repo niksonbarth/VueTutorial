b341dd273c2d96a16b125d19e978be4b12f01f4c
nikson <nikson.barth@benner.com.br>
2017-12-19 13:19:33 -0200

    Aula 6

--- main.js
+++ main.js
@@ -1,37 +1,30 @@
-Vue.component('friend-component',{
-    props:['friend'],
-    filters:{
-        fullName(value){
-            return `${value.first} ${value.last}`;
-        },
-        ageInOneYear(age){
-            return age + 1;
-        }
-    },
-    methods:{
-        decrementAge(friend){
-            friend.age = friend.age - 1;
-        },
-        incrementAge(friend){
-            friend.age = friend.age + 1;
-        },
-    },
-    template: `
-        <div>
-            <h4>{{friend | fullName}}</h4>
-            <h5>age: {{friend.age}}</h5>
-            <button v-on:click="decrementAge(friend)">-</button>
-            <button v-on:click="incrementAge(friend)">+</button>
-            <input v-model="friend.first"/>
-            <input v-model="friend.last"/>
-        </div>
-    `
-})
-
 const app = new Vue({
     el: "#app",
     data: {
-      friends: [],    
+        editFriend: null,
+        friends: [],    
+    },
+    methods:{
+        deleteFriend(id, i){
+            fetch("http://rest.learncode.academy/api/vue-5/friends/" + id, {
+                method: "DELETE"
+            })
+            .then(() => {
+                this.friends.splice(i, 1);
+            })
+        },
+        updateFriend(friend){
+            fetch("http://rest.learncode.academy/api/vue-5/friends/" + friend.id, {
+                body: JSON.stringify(friend),
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then(() => {
+                this.editFriend = null;
+            })
+        }    
     },
     mounted(){
         fetch("http://rest.learncode.academy/api/vue-5/friends")
@@ -42,7 +35,17 @@ const app = new Vue({
     },
     template: `
       <div>
-        <friend-component v-for="item in friends" v-bind:friend="item">        
+        <li v-for="friend, i in friends">
+            <div v-if="editFriend === friend.id">
+                <input v-on:keyup.13="updateFriend(friend)" v-model="friend.name"/>
+                <button v-on:click="updateFriend(friend)">save</button>
+            </div>
+            <div v-else>
+                <button v-on:click="editFriend = friend.id">edit</button>
+                <button v-on:click="deleteFriend(friend.id, i)">x</button>
+                {{friend.name}}
+            </div>
+        </li>       
       </div>
     `
   })
